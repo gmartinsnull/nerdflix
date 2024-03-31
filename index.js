979176677e339b9fb4ffd049bc9c33499a7d95a5
gmartinsnull <gmartins.dev@gmail.com>
2024-03-31 16:26:44 -0700

    database integration; refactoring some APIs to suit new database integration; adding movie creation ejs for better UX

--- index.js
+++ index.js
@@ -1,69 +1,125 @@
 import express from "express";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+  database: "movies",
+  host: "localhost",
+  user: "postgres",
+  password: "admin",
+  port: 5432,
+});
+
+db.connect();
 
 // hashmap of movies. Will be replaced with db eventually
-// todo: replace with database solution
-const movies = {
+let movies = {
   "the lord of the rings": false,
   "the avengers": false,
   "iron man": false,
-  "godzilla": false,
-  "dune": false
+  godzilla: false,
+  dune: false,
 };
+let currentMovieId = 0;
 
 /**
  * http body decoder
  */
 app.use(express.urlencoded({ extended: true }));
 
+async function getCurrentMovie() {
+  const result = await db.query("SELECT * FROM movies WHERE id = $1", [
+    currentMovieId,
+  ]);
+  const movie = result.rows[0];
+  const currentMovie = {
+    id: movie.id,
+    title: movie.title,
+    description: movie.description,
+    year: movie.year,
+    duration: movie.duration,
+    rating: movie.rating,
+    liked: movie.liked,
+  };
+  return currentMovie;
+}
+
 /**
- * like endpoint. Toggles on/off like flag in data set
+ * create endpoint. Takes user to create movie screen/ejs
  */
-app.post("/create", (req, res) => {
-    let title = req.body.title;
-    console.log(title);
-    if (!(title.toLowerCase() in movies)) {
-        movies[title.toLowerCase()] = false;
-        res.render("index.ejs", { title: title, added: 1 }); 
-    } else {
-        res.render("index.ejs", { title: title, added: 2 }); 
-    }
-    console.log("movies",movies);
+app.get("/create", (req, res) => {
+  res.render("new.ejs");
 });
-  
-app.use(express.urlencoded({ extended: true }));
+
+/**
+ * create endpoint. Takes user to create movie screen/ejs
+ */
+app.post("/add", async (req, res) => {
+  console.log("req: ", req.body);
+  const title = req.body.title;
+  const desc = req.body.description;
+  const year = req.body.year;
+  const duration = req.body.duration;
+  const rating = req.body.rating;
+
+  if (title && desc && year && duration && rating) {
+    const result = await db.query(
+      "INSERT INTO movies (title, description, year, duration, rating, liked) VALUES ($1, $2, $3, $4, $5, false)",
+      [title, desc, year, duration, rating]
+    );
+    res.status(201);
+  } else {
+    res.status(400);
+  }
+  res.redirect("/");
+});
 
 /**
  * like endpoint. Toggles on/off like flag in data set
  */
-app.post("/like", (req, res) => {
-    let title = req.body.title;
-    console.log(title);
-    if (title.toLowerCase() in movies) {
-        let toggle = !movies[title.toLowerCase()];
-        movies[title.toLowerCase()] = toggle;
-        if (toggle) {
-            res.render("index.ejs", { title: title, liked: 1 }); 
-        }else {
-            res.render("index.ejs", { title: title, liked: 2 }); 
-        }
-        
-    } 
-    console.log("movies",movies);
-  });
+app.post("/like", async (req, res) => {
+  const currentMovie = await getCurrentMovie();
+  console.log("current: ", currentMovie);
+  try {
+    const result = await db.query(
+      "UPDATE movies SET liked = NOT $1 WHERE id = $2 RETURNING *",
+      [currentMovie.liked, currentMovie.id]
+    );
+    console.log("movie liked: ", result.rows[0]);
+    const liked = result.rows[0].liked;
+    if (liked) {
+      res.render("index.ejs", { title: currentMovie.title, liked: 1 });
+    } else {
+      res.render("index.ejs", { title: currentMovie.title, liked: 2 });
+    }
+  } catch (err) {
+    res.status(500);
+  }
+});
 
 /**
  * search endpoint. returns 1 for success and 2 for "not found"
  */
-app.get("/search", (req, res) => {
+app.get("/search", async (req, res) => {
   let title = req.query.title;
-  console.log(title);
-  if (title.toLowerCase() in movies) {
-    res.render("index.ejs", { title: title, found: 1 }); 
-  } else {
-    res.render("index.ejs", { title: title, found: 2 }); 
+  console.log("title: ", title);
+  try {
+    const result = await db.query(
+      "SELECT * FROM movies WHERE LOWER(title) LIKE '%' || $1 || '%';",
+      [title.toLowerCase().trim()]
+    );
+    const movie = result.rows[0];
+    console.log("movie: ", movie);
+    if (movie) {
+      currentMovieId = movie.id;
+      res.render("index.ejs", { title: movie.title, found: 1 });
+    } else {
+      res.render("index.ejs", { title: movie.title, found: 2 });
+    }
+  } catch (err) {
+    res.render("index.ejs", { title: title, found: 2 });
+    res.status(404);
   }
 });
 
